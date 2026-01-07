c26ff073811684153ef6b39bacf534d396e007e0
xkfwhtjs <apxk0424@gmail.com>
2026-01-07 21:02:48 +0900

    Title: 별표 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/별표 출력하기 2/print-star-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = n; i>=1; i--){
+        for (int j=0; j<i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    // Please write your code here.
+    return 0;
+}
