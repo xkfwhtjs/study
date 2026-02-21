cf498778be844d35c7352245069b37fc7f047c62
xkfwhtjs <apxk0424@gmail.com>
2026-02-21 20:28:09 +0900

    Title: 좌우로 반복하며 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260221/좌우로 반복하며 출력/print-in-left-right-reverse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        
+        if (i % 2 == 0) {
+            for (int j = 1; j <= n; j++) {
+                printf("%d", j);
+            }
+        } 
+        else {}
+            for (int j = n; j >= 1; j--) {
+                printf("%d", j);
+            }
+        }
+        
+        printf("\n"); 
+    }
+
+    return 0;
+}
