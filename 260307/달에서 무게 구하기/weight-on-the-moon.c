7023309f4249398b242aa44c619d870d9b89c5d6
xkfwhtjs <apxk0424@gmail.com>
2026-03-07 16:20:50 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260307/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int mass=13;
+    float gravity=0.165;
+    printf("%d * %6f = %6f", mass, gravity, mass*gravity);
+    // Please write your code here.
+    return 0;
+}
