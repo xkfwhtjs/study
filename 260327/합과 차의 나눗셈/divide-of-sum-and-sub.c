53e557768d46f0fab96a86623145a33a212f98d1
xkfwhtjs <apxk0424@gmail.com>
2026-03-27 11:49:23 +0900

    Title: 합과 차의 나눗셈, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260327/합과 차의 나눗셈/divide-of-sum-and-sub.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d;
+    scanf("%d %d",&a,&b);
+    c = a +b;
+    d = a-b;
+    printf("%.2lf",(double)c/d);
+    // Please write your code here.
+    return 0;
+}
