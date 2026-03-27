fb79900570663df36ea09526c748ef77de753c6c
xkfwhtjs <apxk0424@gmail.com>
2026-03-27 12:14:15 +0900

    Title: 음수 구별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260327/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a<0){
+        printf("%d\n",a);
+        printf("minus");
+    } else{
+        printf("%d",a);
+    }
+    // Please write your code here.
+    return 0;
+}
