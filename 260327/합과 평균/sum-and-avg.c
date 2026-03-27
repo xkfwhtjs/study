8e21cfb668cbd4dbea2fde3932b0fe444558be0d
xkfwhtjs <apxk0424@gmail.com>
2026-03-27 11:54:48 +0900

    Title: 합과 평균, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260327/합과 평균/sum-and-avg.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    double d;
+    scanf("%d %d", &a,&b);
+    c = a+b;
+    d = (double)(a+b)/2;
+    printf("%d %.1f", c,d);
+    // Please write your code here.
+    return 0;
+}
