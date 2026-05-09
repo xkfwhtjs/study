f009d15455722ba7d32ea59a1d27edf021cff67e
xkfwhtjs <apxk0424@gmail.com>
2026-05-09 20:43:06 +0900

    [Passed] 정수의 조건 여부 (8ms, 0MB)

---
+++ 260509/정수의 조건 여부/numbers-condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>=113){
+        printf("1");
+    } else {
+        printf("0");
+    }
+    // Please write your code here.
+    return 0;
+}
