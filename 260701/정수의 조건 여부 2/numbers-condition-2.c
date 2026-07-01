4a83fff998a8457645e4b1d7a3cbfacb3c8fbab7
xkfwhtjs <apxk0424@gmail.com>
2026-07-01 16:01:23 +0900

    [Passed] 정수의 조건 여부 2 (2ms, 0MB)

---
+++ 260701/정수의 조건 여부 2/numbers-condition-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a==5){
+        printf("A");
+    };
+    if(a%2==0){
+        printf("B");
+        
+    };
+
+    // Please write your code here.
+    return 0;
+}
