b1ea8820b71666602361047958964863fde17f68
xkfwhtjs <apxk0424@gmail.com>
2026-07-15 15:44:53 +0900

    [Passed] 두 수의 짝홀 여부 (1ms, 0MB)

---
+++ 260715/두 수의 짝홀 여부/parity-of-two-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d", &a, &b);
+
+    if(a%2==0){
+        printf("even\n");
+    } else{
+        printf("odd\n");
+    }
+
+    if(b%2==0){
+        printf("even\n");
+    }else{
+        printf("odd\n");
+    }
+    // Please write your code here.
+    return 0;
+}
