9d4d0344e64297fdb4bdd53111a7c5f66071948a
xkfwhtjs <apxk0424@gmail.com>
2026-08-08 19:12:27 +0900

    [Passed] 범위 안의 수 (2ms, 0MB)

---
+++ 260808/범위 안의 수/number-in-range.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if(a>=10 && a<=20){
+        printf("yes");
+    }else{
+        printf("no");
+    }
+    return 0;
+}
