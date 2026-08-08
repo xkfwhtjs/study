9c3aac8f0ae7be780c35e0513e7c5b8cfeda6240
xkfwhtjs <apxk0424@gmail.com>
2026-08-08 18:28:58 +0900

    [Passed] 비교 연산 (1ms, 0MB)

---
+++ 260808/비교 연산/comparison-operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%d %d\n", &a,&b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a>b);
+    printf("%d\n",a<=b);
+    printf("%d\n",a<b);
+    printf("%d\n",a==b);
+    printf("%d",a!=b);
+    return 0;
+}
